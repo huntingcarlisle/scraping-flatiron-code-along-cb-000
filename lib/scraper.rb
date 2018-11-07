10f73803345b93191974e1812148e63336f1befa
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-07 07:40:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
   
   def make_courses
     self.get_courses.each{|element| 
-    Course.new(element.css("h2"), element.css("."))
+    Course.new(element.css("h2"), element.css(".date"), element.css(""))
     }
   end
     
